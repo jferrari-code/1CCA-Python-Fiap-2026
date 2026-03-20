43a37742c550d1d758b50a11909b24a1dbbceeb4
jferrari-code <joaopffaian@gmail.com>
2026-03-20 17:26:38 -0300

    exercicios 1 ao 3 + Duvida

---
+++ aula02/exercicios.py
@@ -0,0 +1,39 @@
+# Exercicio 01
+raio = 5
+area = 3.14159 + raio**2
+print(area)
+
+
+
+
+
+# Exercicio 02
+''' VERSAO ERRADA 
+Fahrenheit = input("Temperatura em Fahrenheit: ")
+
+Celsius0 = (Fahrenheit - 32) * 5/9
+
+print(Celsius0)
+
+'''
+Fahrenheit = 68
+Celsius = (Fahrenheit - 32) * 5/9
+
+print(Celsius)
+
+
+
+
+# Exercicio 03
+
+livros = 3
+canetas = 2
+
+total_gasto = (livros * 25) + (canetas * 5)
+
+print(total_gasto)
+
+
+
+# Exercicio 04
+
