42052180f181f85c6058e822c4632d84e4f42f01
jferrari-code <joaopffaian@gmail.com>
2026-03-20 11:31:36 -0300

    Aula 02 Variaveis e operadores

---
+++ aula02/aula02.py
@@ -0,0 +1,36 @@
+print('ola mundo')
+
+
+print(7+4)
+print('7+4')
+print('ola eu tenho' + ' 17 anos') #Concatenacao de strings
+
+#Comentario de 1 linha
+
+'''
+Comentario de muitas linhas
+muitas linhas
+muito mesmo
+
+'''
+
+#variaveis
+nome = "Joao Ferrari" #str
+idade = 17 #int(inteiro)
+peso = 72.1 #float(numero quebrado)
+
+print(nome, idade, peso)
+print(f"oiii {nome}!!!!!")
+
+
+
+
+#input - SIMULACAO DE UM FORMS NO CMD
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(nova_idade)
---
+++ aula02/desafios.py
@@ -0,0 +1,11 @@
+
+#Desafio 01
+
+nome = input("Qual seu nome? ")
+print(f"Ola {nome}, seja bem vindo!!!")
+
+
+
+#Desafio 02
+dia = input("Qual o dia do seu aniversario?")
+mes = input
