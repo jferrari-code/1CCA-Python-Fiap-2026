9eb27492568255739c2889e07099b24ec91d071e
jferrari-code <joaopffaian@gmail.com>
2026-03-20 17:09:06 -0300

    Desafios finalizados

--- aula02/desafios.py
+++ aula02/desafios.py
@@ -7,5 +7,30 @@ print(f"Ola {nome}, seja bem vindo!!!")
 
 
 #Desafio 02
-dia = input("Qual o dia do seu aniversario?")
-mes = input
+
+dia = input("Qual o dia do seu aniversario? ")
+print(f"Certo! Entao voce nasceu no dia {dia}")
+print("Agora, me diga:")
+
+
+mes = input("Qual e o mes do seu aniversario? ")
+print(f"Certo! Entao voce nasceu no mes {mes}")
+print("Agora, me diga:")
+
+
+ano = input("Qual e o ano em que voce nasceu? ")
+print(f"Certo! Entao voce nasceu no ano de {ano}")
+print("Agora, me diga:")
+
+
+print(f"Certo! Entao voce nasceu no dia {dia}, do mes {mes}, do ano de {ano}!")
+input("Correto? ")
+
+
+#Desafio 03
+
+numero1 = 230
+numero2 = 600
+numero3 = numero1 + numero2
+
+print(f"A soma entre os numeros, {numero1} + {numero2} = {numero3}")
