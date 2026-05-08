a0d18df5ffe04070af0b8aca4844317750f7514f
jferrari-code <joaopffaian@gmail.com>
2026-05-08 11:22:24 -0300

    aula05

---
+++ aula05/atividade-nomes.py
@@ -0,0 +1,5 @@
+nomes = ["joao" , "caio" , "paulo" , "pedro"]
+
+for i in range(4):
+    for j in range(i+1, 4):
+        print(nomes[i], nomes[j])
---
+++ aula05/aula05-matrizes.py
@@ -0,0 +1,11 @@
+musicas = [
+    ["Chicago","MJ",],
+    ["Sorry" , "JB",],
+    ["Judas", "LG",]
+]
+
+print(musicas[1][0])
+
+for musica in musicas:
+    print(musica)
+
